a73ac98e27d8ee950cc64bba4fb245fab73d33c3
Liam Ma <liam.q.ma@gmail.com>
2022-04-06 21:40:37 +1000

    Add sentry

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+
+Sentry.init({
+  dsn: "https://9d63122416ab42d8a40e64b8c82ea953@o1191704.ingest.sentry.io/6313174",
+  integrations: [new BrowserTracing()],
+  denyUrls: ["http://localhost:3000"],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
 
 ReactDOM.render(
   <React.StrictMode>
