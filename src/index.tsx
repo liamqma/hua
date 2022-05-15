12b97f6ee1255d149f985a6ef6d6a2cb90c701eb
Liam Ma <liam.q.ma@gmail.com>
2022-05-15 18:26:53 +1000

    remove reportWebVitals

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import * as Sentry from "@sentry/react";
 import { BrowserTracing } from "@sentry/tracing";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 
 Sentry.init({
   dsn: "https://9d63122416ab42d8a40e64b8c82ea953@o1191704.ingest.sentry.io/6313174",
@@ -22,8 +21,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
